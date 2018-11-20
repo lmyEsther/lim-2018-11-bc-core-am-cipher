119284dcadb05c6ecb09655565d4f0ac5fdd0505
lmyEsther <esthercheremo@gmail.com>
2018-11-20 16:44:41 -0500

    Listo ya descifra, gracias Gonzalo por los tips

--- src/index.js
+++ src/index.js
@@ -9,20 +9,37 @@
 //}
  //console.log(letraCifrada('M',4));
 
- function palabraCifrada (palabra, desplazar) {
+ function palabraCifrada (string, offset) {
    let cifrada = '';
 
-   for (let i = 0; i <= palabra.length; i ++){
+   for (let i = 0; i <= string.length; i ++){
+    const nroPalabra = string.charCodeAt(i);
+    const formula = (nroPalabra - 65 + offset) % 26 + 65;
 
-    const nroPalabra = palabra.charCodeAt(i);
-    const formula = (nroPalabra - 65 + desplazar) % 26 + 65;
-        cifrada += String.fromCharCode(formula);
-
-
+    if (nroPalabra === 32){
+      cifrada += ' ';
+    } else {
+      cifrada += String.fromCharCode(formula);
+    }
 
    }
-
-
    return cifrada;
  }
-console.log(palabraCifrada('DESDE', 2));
+console.log(palabraCifrada('LEOMARIYER', 3000));
+
+function palabraDescifrada (string, offset) {
+  let descifrada = '';
+
+  for (let i = 0; i <= string.length; i ++){
+   const nroPalabra = string.charCodeAt(i);
+   const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65; //Listo, lo logre, ya descifra, Gracias Gonzalo por los tips
+
+   if (nroPalabra === 32){
+     descifrada += ' ';
+   } else {
+     descifrada += String.fromCharCode(formula);
+   }
+  }
+  return descifrada;
+}
+console.log(palabraDescifrada('VOYWKBSIOB', 3000))
