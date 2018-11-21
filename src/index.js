a32bde176ea849e554b382375307a6b8faf7aedc
lmyEsther <esthercheremo@gmail.com>
2018-11-21 13:05:07 -0500

    Botones no me funcionan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
- function palabraCifrada (string, offset) {
+const string = document.getElementbyId("texto-ingresado");
+const offset = document.getElemntbyId("offset-ingresado");
+const btnCifrar = document.getElementbyId("btn-cifrar");
+const btnDescifrar = document.getElementbyId("btn-descifrar");
+
+function palabraCifrada (string, offset) {
    let cifrada = '';
 
    for (let i = 0; i <= string.length; i ++){
@@ -14,14 +19,13 @@
    }
    return cifrada;
  }
-console.log(palabraCifrada('LEOMARIYER', 3000));
 
 function palabraDescifrada (string, offset) {
   let descifrada = '';
 
   for (let i = 0; i <= string.length; i ++){
    const nroPalabra = string.charCodeAt(i);
-   const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65; //Listo, lo logre, ya descifra, Gracias Gonzalo por los tips
+   const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65; //Listo, lo logre, ya descifra
 
    if (nroPalabra === 32){
      descifrada += ' ';
@@ -31,4 +35,14 @@ function palabraDescifrada (string, offset) {
   }
   return descifrada;
 }
-console.log(palabraDescifrada('VOYWKBSIOB', 3000))
+
+const mostrarText = document.getElementbyId("mostrar-texto");
+
+function clickCifrar (){
+  mostrarText.innerHTML = palabraCifrada(string.value, parseInt(offset.value));
+}
+function clickDescifrar (){
+  mostrarText.innerHTML = palabraDescrifrada(string.value, parseInt(offset.value));
+}
+btnCifrar.addEventListener('click', clickCifrar)
+btnDescifrar.addEventListener('click', clickDescifrar)
