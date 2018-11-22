10b9ed8eb725bf5c3460f691b7fda9dfb8c5414c
lmyEsther <esthercheremo@gmail.com>
2018-11-22 12:58:26 -0500

    mis funciones en el objeto cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,35 @@
 window.cipher = {
-  // ... 
+  // ...
+  encode: (string, offset) => {
+     let cifrada = '';
+
+     for (let i = 0; i <= string.length; i ++){
+      const nroPalabra = string.charCodeAt(i);
+      const formula = (nroPalabra - 65 + offset) % 26 + 65;
+
+      if (nroPalabra === 32){
+        cifrada += ' ';
+      } else {
+        cifrada += String.fromCharCode(formula);
+      }
+     }
+     return cifrada;
+   },
+
+  decode: (string, offset) => {
+    let descifrada = '';
+
+    for (let i = 0; i <= string.length; i ++){
+     const nroPalabra = string.charCodeAt(i);
+     const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65;
+
+     if (nroPalabra === 32){
+       descifrada += ' ';
+     } else {
+       descifrada += String.fromCharCode(formula);
+     }
+    }
+    return descifrada;
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -5,44 +5,11 @@ let btnDescifrar = document.getElementById("btn-descifrar");
 let vtnCifrar = document.getElementById("ventana-cifrar");
 let vtnDescifrar = document.getElementById("ventana-descifrar");
 
-function cifrar (string, offset) {
-   let cifrada = '';
-
-   for (let i = 0; i <= string.length; i ++){
-    const nroPalabra = string.charCodeAt(i);
-    const formula = (nroPalabra - 65 + offset) % 26 + 65;
-
-    if (nroPalabra === 32){
-      cifrada += ' ';
-    } else {
-      cifrada += String.fromCharCode(formula);
-    }
-   }
-   return cifrada;
- }
-
-function descifrar (string, offset) {
-  let descifrada = '';
-
-  for (let i = 0; i <= string.length; i ++){
-   const nroPalabra = string.charCodeAt(i);
-   const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65;
-
-   if (nroPalabra === 32){
-     descifrada += ' ';
-   } else {
-     descifrada += String.fromCharCode(formula);
-   }
-  }
-  return descifrada;
-}
-
-
 function clickCifrar (){
   const mostrarText = document.getElementById("mostrar-texto");
   const mostrado = document.getElementById("mostrado");
   mostrado.style.visibility="visible";
-  mostrarText.innerHTML = cifrar(string.value, parseInt(offset.value));
+  mostrarText.innerHTML = cipher.encode(string.value, parseInt(offset.value));
 
 
 }
@@ -50,7 +17,7 @@ function clickDescifrar (){
   const mostrarText = document.getElementById("mostrar-texto");
   const mostrado = document.getElementById("mostrado");
   mostrado.style.visibility="visible";
-  mostrarText.innerHTML = descifrar(string.value, parseInt(offset.value));
+  mostrarText.innerHTML = cipher.decode(string.value, parseInt(offset.value));
 }
 
 let contenido = document.getElementById("contenido");
