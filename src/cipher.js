652ffa2a985bc9e3848353b9defbe00622b4a328
lmyEsther <esthercheremo@gmail.com>
2018-11-27 10:43:50 -0500

    cambios en contenido y cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,17 @@
 window.cipher = {
   // ...
   encode: (string, offset) => {
-     let cifrada = '';
+     let cifrada ='';
      let textoMayus = string.toUpperCase();
 
-     for (let i = 0; i <= textoMayus.length; i ++){
+     for (let i = 0; i < textoMayus.length; i ++){
       const nroPalabra = textoMayus.charCodeAt(i);
       const formula = (nroPalabra - 65 + offset) % 26 + 65;
 
       if (nroPalabra === 32){
         cifrada +=' ';
       } else {
-        cifrada += String.fromCharCode(formula);
+        cifrada +=String.fromCharCode(formula);
       }
      }
      return cifrada;
@@ -21,7 +21,7 @@ window.cipher = {
     let descifrada = '';
     let textoMayus = string.toUpperCase();
 
-    for (let i = 0; i <= textoMayus.length; i ++){
+    for (let i = 0; i < textoMayus.length; i ++){
      const nroPalabra = textoMayus.charCodeAt(i);
      const formula = (nroPalabra + 65 - (offset%26)) % 26 + 65;
 
