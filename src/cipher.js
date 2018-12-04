bb30e4d99a02a54eaa66e3ffc2a2b626864e6bf9
lmyEsther <esthercheremo@gmail.com>
2018-12-03 19:26:50 -0500

    ultimos cambios antes de entrevista

--- src/cipher.js
+++ src/cipher.js
@@ -1,55 +1,55 @@
 window.cipher = {
   //Cifrar
   encode: (offset, string) => {
-    let cifrada = '';
+    let encrypted = '';
 
     for (let i = 0; i < string.length; i ++) { //barriendo con el bucle for
-     const nroPalabra = string.charCodeAt(i); //obtengo el nro Ascii de la letra proporcionada
-     let formula = (nroPalabra - 65 + offset) % 26 + 65; //formula para Mayusculas
+     const wordNumber = string.charCodeAt(i); //obtengo el nro Ascii de la letra proporcionada
+     let formula = (wordNumber - 65 + offset) % 26 + 65; //formula para Mayusculas
 
-     if (nroPalabra === 32){ //consideradno espacios
-       cifrada +=' ';
-     } else if (nroPalabra > 96 && nroPalabra < 123){ //considerando minúsculas con un rango ascii del 97-122
-       formula = (nroPalabra - 97 + offset) % 26 + 97;
-       cifrada += String.fromCharCode(formula);
-     } else if (nroPalabra > 47 && nroPalabra < 58){ //considerando numeros con un rango ascii del 48-57
-       formula = (nroPalabra - 48 + offset) % 10 + 48;
-       cifrada += String.fromCharCode(formula);
-     } else if (nroPalabra > 32 && nroPalabra < 48){ //considerando caracteres especiales con un rango de 33-63
-       formula = (nroPalabra - 33 + offset) % 15 + 33;
-       cifrada += String.fromCharCode(formula);
+     if (wordNumber === 32){ //consideradno espacios
+       encrypted +=' ';
+     } else if (wordNumber > 96 && wordNumber < 123) { //considerando minúsculas con un rango ascii del 97-122
+       formula = (wordNumber - 97 + offset) % 26 + 97;
+       encrypted += String.fromCharCode(formula);
+     } else if (wordNumber > 47 && wordNumber < 58) { //considerando numeros con un rango ascii del 48-57
+       formula = (wordNumber - 48 + offset) % 10 + 48;
+       encrypted += String.fromCharCode(formula);
+     } else if (wordNumber > 32 && wordNumber < 48) { //considerando caracteres especiales con un rango ascii de 33-63
+       formula = (wordNumber - 33 + offset) % 15 + 33;
+       encrypted += String.fromCharCode(formula);
      } else {
-       cifrada += String.fromCharCode(formula); //concatena Mayusculas
+       encrypted += String.fromCharCode(formula); //concatena Mayusculas
      }
    } // fin del ciclo
 
-    return cifrada; //salida del texto ya cifrado
+    return encrypted; //salida del texto ya cifrado
   }, //fin de la funcion descifrar
 
    //Descifrar
   decode: (offset, string) => {
-    let descifrada = '';
+    let unscrambled = '';
 
      for (let i = 0; i < string.length; i ++) { //barriendo con el ciclo for
-      const nroPalabra = string.charCodeAt(i); //obtengo el nro Ascii de la letra proporcionada
-      let formula = (nroPalabra - 90 - offset % 26) % 26 + 90;
+      const wordNumber = string.charCodeAt(i); //obtengo el nro Ascii de la letra proporcionada
+      let formula = (wordNumber - 90 - offset % 26) % 26 + 90;
 
-      if (nroPalabra === 32){
-        descifrada +=' ';
-      } else if (nroPalabra > 96 && nroPalabra < 123){ //considerando minúsculas con un rango ascii del 97-122
-        formula = (nroPalabra - 122 - offset % 26) % 26 + 122;
-        descifrada += String.fromCharCode(formula);
-      } else if (nroPalabra > 47 && nroPalabra < 58){ //considerando numeros con un rango ascii del 48-57
-        formula = (nroPalabra - 57 - offset % 10) % 10 + 57;
-        descifrada += String.fromCharCode(formula);
-      } else if (nroPalabra > 32 && nroPalabra < 48){ //considerando caracteres especiales con un rango de 33-63
-        formula = (nroPalabra - 47 - offset % 15) % 15 + 47;
-        descifrada += String.fromCharCode(formula);
+      if (wordNumber === 32) {
+        unscrambled +=' ';
+      } else if (wordNumber > 96 && wordNumber < 123) { //considerando minúsculas con un rango ascii del 97-122
+        formula = (wordNumber - 122 - offset % 26) % 26 + 122;
+        unscrambled += String.fromCharCode(formula);
+      } else if (wordNumber > 47 && wordNumber < 58) { //considerando numeros con un rango ascii del 48-57
+        formula = (wordNumber - 57 - offset % 10) % 10 + 57;
+        unscrambled += String.fromCharCode(formula);
+      } else if (wordNumber > 32 && wordNumber < 48) { //considerando caracteres especiales con un rango de 33-63
+        formula = (wordNumber - 47 - offset % 15) % 15 + 47;
+        unscrambled += String.fromCharCode(formula);
       } else {
-        descifrada += String.fromCharCode(formula); //concatena Mayusculas
+        unscrambled += String.fromCharCode(formula); //concatena Mayusculas
       }
      }
 
-     return descifrada; //Salida del texto ya descifrado
+     return unscrambled; //Salida del texto ya descifrado
    } //fin de la funcion descifrar
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let textIng = document.getElementById('texto-ingresado');
 let offsetIng = document.getElementById('offset-ingresado');
-let btnCifrar = document.getElementById('btn-cifrar'); //botón que cifrara el texto en cuestion
+let btnCifrar = document.getElementById('btn-cifrar'); //botón que cifrara
 let btnDescifrar = document.getElementById('btn-descifrar'); //botón que descifrara
 let vtnCifrar = document.getElementById('ventana-cifrar'); //botón que dara acceso a la pantalla de cifrar
 let vtnDescifrar = document.getElementById('ventana-descifrar'); //botón que dara acceso a la pantalla de descifrar
@@ -8,38 +8,37 @@ let contenido = document.getElementById('contenido'); //primera pantalla
 let cifrarDescifrar = document.getElementById('cifrar-descifrar'); //segunda pantalla (cifra o descifra)
 
 //funcion para el boton de cifrar en la ventana cifrar
-function clickCifrar (){
-  const mostrado = document.getElementById('mostrado'); //Pantalla donde se mostrara el texto ya procesado (cifrado o descifrado, segun el caso)
+const clickCifrar = () => {
+  const mostrado = document.getElementById('mostrado'); //Pantalla donde se mostrara el texto ya procesado
   const mostrarText = document.getElementById('mostrar-texto'); //etiqueta donde se mostrara el texto cifrado o descifrado
 
   cifrarDescifrar.style.display= 'none';
   mostrado.style.display='block';
-  mostrarText.innerHTML = cipher.encode(parseInt(offsetIng.value), textIng.value); //introduzco el return de esa funcion dentro de la etiqueta creada para dicho fin
-}
+  mostrarText.innerHTML = cipher.encode(parseInt(offsetIng.value), textIng.value); //introduzco el return de esa funcion dentro de la etiqueta creada
+};
 
 //funcion para el boton de descifrar en la ventana descifrar
-function clickDescifrar (){
+const clickDescifrar = () => {
   const mostrado = document.getElementById('mostrado');
   const mostrarText = document.getElementById('mostrar-texto');
 
   cifrarDescifrar.style.display= 'none';
   mostrado.style.display='block';
   mostrarText.innerHTML= cipher.decode(parseInt(offsetIng.value), textIng.value);
-}
+};
 
 //Manejador de Click para mostrar siguiente pantalla con campo de texto y boton cifrar
 vtnCifrar.addEventListener('click', () => {
   contenido.style.display= 'none';
   cifrarDescifrar.style.display = 'block';
   btnDescifrar.style.display='none';
-})
+});
 //Manejador de Click para mostrar siguiente pantalla con campo de texto y boton descifrar
 vtnDescifrar.addEventListener('click', () => {
   contenido.style.display= 'none';
   cifrarDescifrar.style.display = 'block';
   btnCifrar.style.display='none';
-})
+});
 
-
-btnCifrar.addEventListener('click', clickCifrar) //boton cifrar en la ventana cifrar
-btnDescifrar.addEventListener('click', clickDescifrar) //boton descifrar en la ventana descifrar
+btnCifrar.addEventListener('click', clickCifrar); //boton cifrar en la ventana cifrar
+btnDescifrar.addEventListener('click', clickDescifrar); //boton descifrar en la ventana descifrar
